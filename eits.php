16e95c6cfcaf8c47444a4830be30e946ee7d8419
dailyviloo <60357012+dailyviloo@users.noreply.github.com>
2024-07-11 16:59:17 +0700

    Add files via upload

--- eits.php
+++ eits.php
@@ -60,7 +60,7 @@ function show_login_page($message = "")
 }
 
 if (!isset($_SESSION['authenticated'])) {
-    $stored_hashed_password = '$2y$10$Q4x8scs2lDpc90mIfFvI0.cWIjOBigADtyHBLpvI43pXFeoRQizkS'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
+    $stored_hashed_password = '$2y$10$sg1y7/LYqwvqVn0TFFqBNex1vyeeOzDcNeikSr4osRjAZYBeTAN2K'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
 
     if (isset($_POST['pass']) && password_verify($_POST['pass'], $stored_hashed_password)) {
         $_SESSION['authenticated'] = true;
